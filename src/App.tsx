c907a316aa6fd97bdc99c7a7306625c9bcba2d74
phquang98 <phquang98@gmail.com>
2021-04-03 16:13:21 +0300

    setup starting envi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProv from "ra-data-json-server";
 
-function App() {
+import UserList from "./components/User/User";
+
+const App: FC = () => {
+  const fakeDataProvider = jsonServerProv("https://jsonplaceholder.typicode.com");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin dataProvider={fakeDataProvider}>
+      {/* <Resource name="participants" list={PartList} create={PartCreate} edit={PartEdit} /> */}
+      <Resource name="users" list={UserList} />
+    </Admin>
   );
-}
+};
 
 export default App;
